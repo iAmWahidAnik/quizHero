c2ed00da2f56c527ac6a76a866126ea690c5289e
wahidanik <wahid.anik@outlook.com>
2023-03-15 21:29:10 +0600

    question displayed

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -57,14 +57,14 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
   data.forEach((quiz, i) => {
+    // console.log(quiz, 'eda hoilo iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -73,8 +73,8 @@ const displayQuiz = (data) => {
   });
 };
 
-// EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+// EventListener for quiz submit button addEventListener
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
